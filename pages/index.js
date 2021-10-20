092c8ec94283f9b77a86d8b4b1f2d015f4c4769e
viranmalaka <malaka.14@cse.mrt.ac.lk>
2021-10-20 12:16:00 +0530

    add location numbers

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import GoogleMapReact from "google-map-react";
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-const AnyReactComponent = ({ text }) => <div className={styles.marker}>{text}</div>;
+const AnyReactComponent = ({ text }) => (
+  <div className={styles.marker}>{text}</div>
+);
 
 export default function Home() {
-
   const [location, setLoc] = useState({
     lat: 6.7,
-    lng: 80.3
+    lng: 80.3,
   });
   const [update, setUpdating] = useState(false);
 
   useEffect(() => {
-    if (navigator.geolocation){
+    if (navigator.geolocation) {
       navigator.geolocation.watchPosition((position) => {
         console.log(position.coords);
         setUpdating(true);
@@ -26,21 +27,25 @@ export default function Home() {
         }, 1000);
       });
     } else {
-      console.log('no geo location');
+      console.log("no geo location");
     }
   }, []);
 
   return (
     <div className={styles.container}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyDQoE5Fwwwt2LqvgzGin7YnLrKcDsah92c' }}
+        bootstrapURLKeys={{ key: "AIzaSyDQoE5Fwwwt2LqvgzGin7YnLrKcDsah92c" }}
         defaultCenter={{
           lat: location.lat,
           lng: location.lng,
         }}
         defaultZoom={11}
       >
-        <AnyReactComponent lat={location.lat} lng={location.lng} text={update ? 'Updating' : 'Here'} />
+        <AnyReactComponent
+          lat={location.lat}
+          lng={location.lng}
+          text={update ? "Updating" : `Here (${location.lat}, ${location.lng})`}
+        />
       </GoogleMapReact>
     </div>
   );
